1443a74659bb63e6d159d6405e264f731ebf2b43
Ben Kantor <bkantor01@gmail.com>
2014-10-07 15:33:01 -0400

    Adds temporary splash page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Brickhack::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'application#application'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
