ccf5f7c64cec532c5b6563c4916e9c94ec83f084
Stuart Olivera <stuart@stuartolivera.com>
2014-10-11 16:06:03 -0400

    Initialized with rails new ./

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrickhackIo::Application.config.secret_token = '692409a8824728e938ce3f194e92585a6930d02f5f9e771c47959232acc3e0122e0f7d5ea75b7470a7d4fcd04dc96696a8b9a4f686bac7c1093b74b2ee35de95'
