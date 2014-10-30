4944f4208009f59c4ac43e8e7371504460a0c788
Colden Cullen <ColdenCullen@gmail.com>
2014-10-30 00:48:51 -0400

    Merged Openshift Rails demo app for config settings

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,14 @@
+
+require File.join(Rails.root,'lib','openshift_secret_generator.rb')
 # Be sure to restart your server when you modify this file.
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-BrickhackIo::Application.config.secret_token = '692409a8824728e938ce3f194e92585a6930d02f5f9e771c47959232acc3e0122e0f7d5ea75b7470a7d4fcd04dc96696a8b9a4f686bac7c1093b74b2ee35de95'
+
+# Set token based on intialize_secret function (defined in initializers/secret_generator.rb)
+BrickhackIo::Application.config.secret_token = initialize_secret(
+  :token,
+  '335a4e365ef2daeea969640d74e18f0e3cd9fae1abd8f4125691a880774ea6d456a29c0831aa6921bf86a710fe555e916f0673f5657619ec9df22e0409bec345'
+)
