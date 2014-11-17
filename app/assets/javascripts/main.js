f65beaf4662024af4ea074ce08777b72408d6c54
Nate Perry <ndp4570@g.rit.edu>
2014-11-17 02:31:53 -0500

    cleaned up styling, adjust scrolling

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,10 +2,13 @@ $(document).ready(function () {
 
   $('.nav-link, .scroll-to').on('click', function (e) {
     e.preventDefault();
-    var id = $(e.currentTarget).attr('href')
-    var target = $(id);
+    $('.active').removeClass('active');
+    $(e.currentTarget).addClass('active');
+    var id = $(e.currentTarget).attr('href');
+    var $target = $(id);
+    $target.addClass('active');
     $('html,body').animate({
-      scrollTop: (target.offset().top)
+      scrollTop: ($target.offset().top)
     }, 1000);
   });
 
