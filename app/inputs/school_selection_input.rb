4ff064022c0c6fc5b8be6cd2d061ddb6d1b38fbc
Stuart Olivera <stuart@stuartolivera.com>
2014-12-22 19:41:08 -0500

    Enabled special attributes for school input

--- app/inputs/school_selection_input.rb
+++ app/inputs/school_selection_input.rb
@@ -2,7 +2,7 @@ class SchoolSelectionInput < SimpleForm::Inputs::Base
   def input
     input_name = attribute_name.to_s.gsub(/_id/, '_name')
     value = @builder.object.send(attribute_name).blank? ? '' : @builder.object.school.name
-    text_field_options = { data: { school_picker: true }, name: "#{object_name}[#{input_name}]", value: value}
+    text_field_options = input_html_options.merge({ data: { school_picker: true }, name: "#{object_name}[#{input_name}]", value: value})
     template.text_field(input_name, value, text_field_options)
   end
 end
