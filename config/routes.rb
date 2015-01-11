b2b545a573cb9734706187d81779fb3b3ee9a620
Nate Perry <ndp4570@g.rit.edu>
2015-01-11 17:51:37 -0500

    added new error pages

--- config/routes.rb
+++ config/routes.rb
@@ -68,4 +68,8 @@ BrickhackIo::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
+  unless Rails.application.config.consider_all_requests_local
+    match '*not_found', to: 'errors#error_404'
+  end
 end
