1a186d894d3c810d0a99ba6a5abf1d551dd244b1
Stuart Olivera <stuart@stuartolivera.com>
2015-01-11 01:11:47 -0500

    Included Devise in tests

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should "downcase emails" do
+    s = build(:user, email: "Test@ExAmPlE.cOm")
+    assert_equal "test@example.com", s.email
+  end
+
+  should allow_value("test@example.com").for(:email)
+  should_not allow_value("abcd").for(:email)
 end
