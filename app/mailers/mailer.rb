41cfa3bcbcf0e492be996fe0074efb573838578d
Stuart Olivera <stuart@stuartolivera.com>
2015-01-11 03:31:56 -0500

    Updated email settings

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -3,7 +3,7 @@ class Mailer < ActionMailer::Base
 
   def application_confirmation_email(questionnaire_id)
     @questionnaire = Questionnaire.find(questionnaire_id)
-    mail(to: pretty_email(@questionnaire.full_name, @questionnaire.email), subject: "[BrickHack] Application Received")
+    mail(to: pretty_email(@questionnaire.full_name, @questionnaire.user.email), subject: "[BrickHack] Application Received")
   end
 
   private
