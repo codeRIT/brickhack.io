b99929363f18533b7d531f853a67c75051a4bf03
Nate Perry <ndp4570@g.rit.edu>
2015-01-10 19:39:27 -0500

    fixed sidebar links

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,18 +1,21 @@
 $(document).ready(function () {
 
-  $('.nav-link, .scroll-to').on('click', function (e) {
-    e.preventDefault();
-    $('.active').removeClass('active');
-    $(e.currentTarget).addClass('active');
-    var id = $(e.currentTarget).attr('href');
-    var $target = $(id);
-    $target.addClass('active');
-    $('html,body').animate({
-      scrollTop: ($target.offset().top)
-    }, 1000);
-  });
-
   var $sidebar = $('.sidebar');
+
+  if ( $sidebar.hasClass('home') ) {
+    $('.nav-link, .scroll-to').on('click', function (e) {
+      e.preventDefault();
+      $('.active').removeClass('active');
+      $(e.currentTarget).addClass('active');
+      var id = $(e.currentTarget).attr('href');
+      id = id.substring(1);
+      var $target = $(id);
+      $target.addClass('active');
+      $('html,body').animate({
+        scrollTop: ($target.offset().top)
+      }, 1000);
+    });
+  }
 
   $('#sidebar-toggle').on('click', toggleSidebar);
 
