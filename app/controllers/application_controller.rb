1bc007f6c0632dd0a93ebda65c1fd692c3eb4c27
Stuart Olivera <stuart@stuartolivera.com>
2015-01-15 16:53:41 -0500

    Improved manage & admin integration, added nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def after_sign_in_path_for(resource)
-    if current_user.questionnaire() === nil
+    if current_user.admin?
+      manage_root_path
+    elsif current_user.questionnaire === nil
       new_questionnaire_path
     else
-      @questionnaire = current_user.questionnaire()
-      questionnaire_path(current_user.questionnaire())
+      @questionnaire = current_user.questionnaire
+      questionnaire_path(current_user.questionnaire)
     end
   end
 end
