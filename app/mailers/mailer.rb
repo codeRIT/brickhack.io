a59c93ef39f2f6fc025e5c6d96d061b957608828
Stuart Olivera <stuart@stuartolivera.com>
2015-02-21 17:34:08 -0500

    Consolidated email subject base

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -3,11 +3,19 @@ class Mailer < ActionMailer::Base
 
   def application_confirmation_email(questionnaire_id)
     @questionnaire = Questionnaire.find(questionnaire_id)
-    return unless @questionnaire.user.present?
-    mail(to: pretty_email(@questionnaire.full_name, @questionnaire.user.email), subject: "[BrickHack] Application Received")
+    return unless @questionnaire.present? && @questionnaire.user.present?
+    mail(
+      to: pretty_email(@questionnaire.full_name, @questionnaire.user.email),
+      subject: "[#{subject_base}] Application Received"
+    )
+  end
   end
 
   private
+
+  def subject_base
+    "BrickHack"
+  end
 
   def pretty_email(name, email)
     return email if name.blank?
