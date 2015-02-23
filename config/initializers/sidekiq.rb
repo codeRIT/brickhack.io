0a1d87614d269c6ab9ca345010423db88cb6927b
Stuart Olivera <stuart@stuartolivera.com>
2015-02-22 21:32:03 -0500

    Restored default sidekiq support

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://:#{ENV['REDIS_PASSWORD']}@#{ENV['OPENSHIFT_REDIS_HOST']}:#{ENV['OPENSHIFT_REDIS_PORT']}" }
-end
+if Rails.env.production? || Rails.env.staging?
+  Sidekiq.configure_server do |config|
+    config.redis = { url: "redis://:#{ENV['REDIS_PASSWORD']}@#{ENV['OPENSHIFT_REDIS_HOST']}:#{ENV['OPENSHIFT_REDIS_PORT']}" }
+  end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://:#{ENV['REDIS_PASSWORD']}@#{ENV['OPENSHIFT_REDIS_HOST']}:#{ENV['OPENSHIFT_REDIS_PORT']}" }
+  Sidekiq.configure_client do |config|
+    config.redis = { url: "redis://:#{ENV['REDIS_PASSWORD']}@#{ENV['OPENSHIFT_REDIS_HOST']}:#{ENV['OPENSHIFT_REDIS_PORT']}" }
+  end
 end
