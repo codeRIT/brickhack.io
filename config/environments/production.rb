31880bc54d7409c13f3942ebd508b90a362e7e7b
Stuart Olivera <stuart@stuartolivera.com>
2015-03-03 18:57:55 -0500

    Fixed asset paths & vendor asset precompilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ BrickhackIo::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( manage.css )
+  config.assets.precompile += %w( manage.css vendor/*.js vendor/*.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
