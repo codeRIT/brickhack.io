ec2253ff3842388d06a0e3a6bbd50c04d761bd84
Stuart Olivera <stuart@stuartolivera.com>
2015-03-25 22:35:58 -0400

    Enabled redirection to previous urls after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def after_sign_in_path_for(resource)
-    if current_user.admin?
+    stored_location = stored_location_for(resource)
+    if stored_location
+      stored_location
+    elsif current_user.admin?
       manage_root_path
     elsif current_user.questionnaire === nil
       new_questionnaire_path
