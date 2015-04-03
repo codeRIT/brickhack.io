ebf056e29d8e571697d287694e50312ea02247c4
Stuart Olivera <stuart@stuartolivera.com>
2015-04-03 19:47:25 -0400

    Moved dashbaord map source to external file

---
+++ app/assets/javascripts/manage/map.js
@@ -0,0 +1,52 @@
+var width = 950,
+height = 600;
+
+var appsById = d3.map();
+
+var formatNumber = d3.format(",.0f");
+
+var path = d3.geo.path()
+    .projection(null);
+
+var radius = d3.scale.sqrt()
+    .domain([0, 50])
+    .range([0, 25]);
+
+var svg = d3.select("#chart").append("svg:svg")
+    .attr("width", width)
+    .attr("height", height);
+
+queue()
+    .defer(d3.json, "/us.json")
+    .defer(d3.tsv, "/manage/dashboard/map_data.tsv", function(d) { appsById.set(d.id, +d.apps); })
+    .await(ready);
+
+function ready(error, us) {
+  if (error) return console.error(error);
+
+  svg.append("path")
+      .datum(topojson.feature(us, us.objects.nation))
+      .attr("class", "land")
+      .attr("d", path);
+
+  svg.append("path")
+      .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+      .attr("class", "border border--state")
+      .attr("d", path);
+
+  svg.append("g")
+      .attr("class", "bubble")
+    .selectAll("circle")
+      .data(topojson.feature(us, us.objects.counties).features
+        .sort(function(a, b) { return (appsById.get(b.id) || 0) - (appsById.get(a.id) || 0); }))
+    .enter().append("circle")
+      .attr("transform", function(d) { return "translate(" + path.centroid(d) + ")"; })
+      .attr("r", function(d) { return radius(appsById.get(d.id) || 0); })
+    .append("title")
+      .text(function(d) {
+        return d.properties.name
+            + "\nApplications: " + formatNumber(appsById.get(d.id) || 0);
+      });
+};
+
+d3.select(self.frameElement).style("height", height + "px");
