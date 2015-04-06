267c140bd6c36b4a1c54feac0b05356280a0121b
Stuart Olivera <stuart@stuartolivera.com>
2015-04-06 11:30:15 -0400

    Moved Highcharts options to global config
    Reduces redundant data in views & refresh requests

--- app/assets/javascripts/manage/application.js
+++ app/assets/javascripts/manage/application.js
@@ -17,6 +17,25 @@ $(document).ready(function () {
           for(var i=this.yData.length; i--;) { total += this.yData[i]; };
           return this.name + ': ' + total;
        }
+    },
+    chart: {
+      backgroundColor: "#f9f6f3"
+    },
+    tooltip: {
+      shared: true
+    },
+    plotOptions: {
+      series: {
+        animation: false
+      },
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: true,
+          format: '<b>{point.name}</b>: {point.y}'
+        }
+      }
     }
   });
 
--- config/initializers/chartkick.rb
+++ config/initializers/chartkick.rb
@@ -1,25 +1,4 @@
 Chartkick.options = {
   height: "165px",
-  colors: ["#4886C2", "#62A0DC", "#7BB9F5", "#95D3FF", "#AEECFF", "#C7FFFF"],
-  library: {
-    chart: {
-      backgroundColor: "#f9f6f3"
-    },
-    tooltip: {
-      shared: true
-    },
-    plotOptions: {
-      series: {
-        animation: false
-      },
-      pie: {
-        allowPointSelect: true,
-        cursor: 'pointer',
-        dataLabels: {
-          enabled: true,
-          format: '<b>{point.name}</b>: {point.y}'
-        }
-      }
-    }
-  }
+  colors: ["#4886C2", "#62A0DC", "#7BB9F5", "#95D3FF", "#AEECFF", "#C7FFFF"]
 }
