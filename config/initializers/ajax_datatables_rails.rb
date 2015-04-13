0d1688122df861d28b16571175666fae70b6d858
Stuart Olivera <stuart@stuartolivera.com>
2015-04-12 22:56:02 -0400

    Normalized AjaxDatatablesRails db adapter to msyql
    sqlite no longer used in development or testing

--- config/initializers/ajax_datatables_rails.rb
+++ config/initializers/ajax_datatables_rails.rb
@@ -1,8 +1,4 @@
 AjaxDatatablesRails.configure do |config|
   # available options for db_adapter are: :pg, :mysql2, :sqlite3
-  if Rails.env.development? || Rails.env.test?
-    config.db_adapter = :sqlite3
-  else
-    config.db_adapter = :mysql2
-  end
+  config.db_adapter = :mysql2
 end
