982abf76b0c720fe878b5105f547d56950df45d1
Nate Perry <ndp4570@g.rit.edu>
2015-04-17 01:18:41 -0400

    added mentor list

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -11,6 +11,10 @@ $(document).ready(function () {
       items: 3,
       singleItem: true
     });
+    $('.mentor-list.carousel').owlCarousel({
+      items: 3,
+      singleItem: true
+    });
   }
 
   if ( $sidebar.hasClass('home') ) {
