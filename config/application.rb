8c5b2a33f2500691fe0305d769f286e0bdfd9bce
Stuart Olivera <stuart@stuartolivera.com>
2015-04-30 17:04:22 -0400

    Added skylight config

--- config/application.rb
+++ config/application.rb
@@ -60,5 +60,7 @@ module BrickhackIo
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.skylight.probes = %w(redis)
   end
 end
