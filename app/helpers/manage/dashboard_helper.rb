8e4ede20050dfaf44c81d0de134f092c62e1eff9
Stuart Olivera <stuart@stuartolivera.com>
2015-10-19 00:43:55 -0400

    Added caching for dashboard map data

--- app/helpers/manage/dashboard_helper.rb
+++ app/helpers/manage/dashboard_helper.rb
@@ -1,3 +1,7 @@
 module Manage::DashboardHelper
-
+  def cache_key_for_questionnaires
+    count          = Questionnaire.count
+    max_updated_at = Questionnaire.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "questionnaires/all-#{count}-#{max_updated_at}"
+  end
 end
