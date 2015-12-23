b93820a19db0e0689acc8a74e3a9e191969394a9
Stuart Olivera <stuart@stuartolivera.com>
2015-12-23 01:55:03 -0500

    Removed mail_preview from coverage reports

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,7 @@ if ["manual", "travis"].include?(ENV["RUN_COVERAGE"])
   require 'simplecov'
   require 'codeclimate-test-reporter' if ENV["RUN_COVERAGE"] == "travis"
   SimpleCov.add_filter 'vendor/'
+  SimpleCov.add_filter 'app/mailers/mail_preview.rb'
   if ENV["RUN_COVERAGE"] == "travis"
     SimpleCov.formatters = []
     SimpleCov.start CodeClimate::TestReporter.configuration.profile
