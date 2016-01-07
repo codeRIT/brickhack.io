afebc62bb3843ae30ee04f021a423f59ddb84211
Stuart Olivera <stuart@stuartolivera.com>
2016-01-07 18:01:30 -0500

    Updated custom inputs for SimpleForm 3

--- app/inputs/deletable_attachment_input.rb
+++ app/inputs/deletable_attachment_input.rb
@@ -1,7 +1,8 @@
 class DeletableAttachmentInput < SimpleForm::Inputs::FileInput
-  def input
+  def input(wrapper_options)
+    merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
     out = ''
-    out << @builder.file_field(attribute_name, input_html_options)
+    out << @builder.file_field(attribute_name, merged_input_options)
     if object.send("#{attribute_name}?")
       out << @builder.input("delete_#{attribute_name}", as: :boolean, label: "Remove?")
     end
