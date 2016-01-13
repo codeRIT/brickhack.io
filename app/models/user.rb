9495c1263ab52e1dc5d855b18c525e95babcdc01
Stuart Olivera <stuart@stuartolivera.com>
2016-01-12 20:44:03 -0500

    Fixed UID for My MLH users

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,6 +33,7 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.uid               = auth.info.uid
       user.email             = auth.info.email
       user.password          = Devise.friendly_token[0,20]
     end
