74a8a1506b8525f9a414854e5b8e39c196c68d9c
Stuart Olivera <stuart@stuartolivera.com>
2016-01-31 00:01:53 -0500

    Fixed home js error

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -83,7 +83,7 @@ ready = function() {
     recalc();
 
     // smooth scrolling
-    $('a[href*=#]:not([href=#])').click(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
