24dee3f4933927986e1dcec908d3a1f5efd29726
Stuart Olivera <stuart@stuartolivera.com>
2016-02-23 17:21:07 -0500

    Added dynamic page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+  def title(page_title)
+    content_for(:title) { page_title + " - BrickHack" }
+    page_title
+  end
+
   def btn_link_to(name, path, options = {})
     options[:class] ? options[:class] += " button" : options[:class] = "button"
     link_to(name, path, options)
