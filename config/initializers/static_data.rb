600bb5576cc2af1888550e5a497547f5cad34d8b
Stuart Olivera <stuart@stuartolivera.com>
2016-12-08 01:34:11 -0800

    Style fixes

--- config/initializers/static_data.rb
+++ config/initializers/static_data.rb
@@ -1,6 +1,7 @@
-STATIC_DATA = {}
-
+static_data = {}
 Dir["#{Rails.root}/config/data/**/*.yml"].each do |file|
   basename = File.basename(file, ".yml")
-  STATIC_DATA[basename] = YAML.load_file(file)
+  static_data[basename] = YAML.load_file(file)
 end
+
+STATIC_DATA = static_data.freeze
