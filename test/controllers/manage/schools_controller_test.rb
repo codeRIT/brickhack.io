7af00f6c5e284c03caa651a4048234803df37c17
Stuart Olivera <stuart@stuartolivera.com>
2017-02-09 12:57:53 -0500

    Fix broken test

--- test/controllers/manage/schools_controller_test.rb
+++ test/controllers/manage/schools_controller_test.rb
@@ -272,7 +272,7 @@ class Manage::SchoolsControllerTest < ActionController::TestCase
     end
 
     should "merge but not delete school if it contains questionnaires" do
-      create(:school, name: "My Test School")
+      school = create(:school, name: "My Test School")
       @school.increment(:questionnaire_count, 4)
       @school.save
       create(:questionnaire, school_id: @school.id)
