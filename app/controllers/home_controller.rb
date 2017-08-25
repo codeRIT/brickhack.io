d38ca3c74ddc5d5f57090a0aa03856c87236fd8a
dquach217 <dq6180@rit.edu>
2017-08-25 14:31:13 -0500

    fix merge conflict

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,10 +10,7 @@ class HomeController < ApplicationController
   def design_category
     render layout: "blank"
   end
-<<<<<<< HEAD
 
   def api_list
     render layout: "blank"
-=======
->>>>>>> 8a122f0c100dceb953578f3c45ba95bd63a4cd89
-end
+  end
