e053dad9cd7b71e05d52f13a685073f63e4067ee
dquach217 <dq6180@rit.edu>
2017-09-26 20:50:59 -0500

    change the config so html content can be seen

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,8 +30,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   # MailCatcher
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { address: "localhost", port: 1025 }
+  config.action_mailer.delivery_method = :sparkpost
 
   # Required for Devise
   config.action_mailer.default_url_options = { host: 'localhost:3000', protocol: 'http' }
--- config/initializers/sparkpost_rails.rb
+++ config/initializers/sparkpost_rails.rb
@@ -3,4 +3,6 @@ SparkPostRails.configure do |c|
   c.track_opens = true
   c.track_clicks = true
   c.campaign_id = 'BrickHack 4'
+  c.inline_css = true
+  c.html_content_only = true
 end
