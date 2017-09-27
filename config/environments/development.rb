d8f2d8adf242c070dcfe136a93a6016ef246e349
dquach217 <dq6180@rit.edu>
2017-09-26 22:19:06 -0500

    change sparkpost to smtp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   # MailCatcher
-  config.action_mailer.delivery_method = :sparkpost
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { address: "localhost", port: 1025 }
 
   # Required for Devise
