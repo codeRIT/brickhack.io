b6464000b158a8bf641a69aeb7cce773739a73a4
Stuart Olivera <stuart@stuartolivera.com>
2017-10-10 21:09:20 -0400

    Route cleanup & home controller tests (#533)
    * Provide better docs in config/routes.rb
    
    * Add basic tests for apilist and design_category pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
-  get "/event", to: "home#event"
-  get "/apilist", to: "home#apilist"
-  get "home/index"
-  get "/design_category", to: "home#design_category"
+  # Pages
+  get '/apilist', to: 'home#apilist'
+  get '/design_category', to: 'home#design_category'
+  # get '/', to: 'home#index' is taken care of by "root" below
+
+  # Homepage
   root to: 'home#index'
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -5,4 +5,14 @@ class HomeControllerTest < ActionController::TestCase
     get :index
     assert_response :success
   end
+
+  test "should get design_category" do
+    get :design_category
+    assert_response :success
+  end
+
+  test "should get apilist" do
+    get :apilist
+    assert_response :success
+  end
 end
