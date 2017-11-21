2cc0c9a9af5ff55cdb1860cd547b8bbfb404f366
David Quach <dq6180@rit.edu>
2017-11-20 22:13:36 -0600

    Comingsoon Page (#542)
    * made some changes to coming soon removed nav because it would not render
    
    * add assets
    
    * commented out content and added mlh 2018 banner

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
   test "should get index" do
-    get :index
+    get :comingsoon
     assert_response :success
   end
 
