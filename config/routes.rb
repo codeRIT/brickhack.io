a28a21d36634631d537099b042d00c8facddcccb
dquach217 <dq6180@rit.edu>
2017-12-10 14:31:09 -0600

    add logo, change stylesheet, change nav, change index, routes, and the yml file

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   # get '/', to: 'home#index' is taken care of by "root" below
 
   # Homepage
-  # root to: 'home#index'
-  root to: 'home#comingsoon'
+   root to: 'home#index'
+  # root to: 'home#comingsoon'
 end
