ce21def0b82996f3a84c40c07fa4583aeaf6f969
Stuart Olivera <stuart@stuartolivera.com>
2017-12-14 20:52:11 -0500

    Add /comingsoon route

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   # Pages
   get '/apilist', to: 'home#apilist'
   get '/design_category', to: 'home#design_category'
+  get '/comingsoon', to: 'home#comingsoon'
   # get '/', to: 'home#index' is taken care of by "root" below
 
   # Homepage
-   root to: 'home#index'
+  root to: 'home#index'
   # root to: 'home#comingsoon'
 end
