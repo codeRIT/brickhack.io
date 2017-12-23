cbcc5e8dfb962914bb505b2fc41a931bd280ec12
Stuart Olivera <stuart@stuartolivera.com>
2017-12-23 13:45:24 -0500

    Simplify layouts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,13 @@
 class HomeController < ApplicationController
   def index
-    render layout: "blank"
   end
 
   def comingsoon
-    render layout: "blank"
   end
 
   def design_category
-    render layout: "blank"
   end
 
   def apilist
-    render layout: "api"
   end
 end
