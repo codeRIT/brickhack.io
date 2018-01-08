9cdf5243a7f99ccd9de7a0067b08a5c6bba21e76
Stuart Olivera <stuart@stuartolivera.com>
2018-01-08 16:46:09 -0500

    Notify user of incomplete application or RSVP (#601)
    * Include flashes on homepage
    
    Such as sign out confirmation
    
    * Refactor home_controller_test & restore index test
    
    * Notify user of incomplete application on homepage
    
    * Notify user of incomplete RSVP
    
    * Wording fixes
    
    * Add comment about .flashes padding

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,18 +1,82 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  test "should get index" do
+  should "render index successfully" do
+    get :index
+    assert_response :success
+  end
+
+  should "render comingsoon successfully" do
     get :comingsoon
     assert_response :success
   end
 
-  test "should get design_category" do
+  should "render design_category successfully" do
     get :design_category
     assert_response :success
   end
 
-  test "should get apilist" do
+  should "render apilist successfully" do
     get :apilist
     assert_response :success
+  end
+
+  context "while not authenticated" do
+    should "not display incomplete notice" do
+      get :index
+      assert_select ".flashes", false
+    end
+  end
+
+  context "while authenticated with a complete questionnaire" do
+    setup do
+      @questionnaire = create(:questionnaire)
+      @request.env["devise.mapping"] = Devise.mappings[:admin]
+      sign_in @questionnaire.user
+    end
+
+    should "not display incomplete notice" do
+      get :index
+      assert_select ".flashes", false
+    end
+  end
+
+  context "while authenticated with an accepted questionnaire" do
+    setup do
+      @questionnaire = create(:questionnaire, acc_status: 'accepted')
+      @request.env["devise.mapping"] = Devise.mappings[:admin]
+      sign_in @questionnaire.user
+    end
+
+    should "display RSVP notice" do
+      get :index
+      assert_select ".flashes", /RSVP now/
+    end
+  end
+
+  context "while authenticated with an RSVP'd questionnaire" do
+    setup do
+      @questionnaire = create(:questionnaire, acc_status: 'rsvp_confirmed')
+      @request.env["devise.mapping"] = Devise.mappings[:admin]
+      sign_in @questionnaire.user
+    end
+
+    should "not display RSVP notice" do
+      get :index
+      assert_select ".flashes", false
+    end
+  end
+
+  context "while authenticated with an incomplete questionnaire" do
+    setup do
+      @user = create(:user)
+      @request.env["devise.mapping"] = Devise.mappings[:admin]
+      sign_in @user
+    end
+
+    should "display incomplete notice" do
+      get :index
+      assert_select ".flashes", /Your application is incomplete/
+    end
   end
 end
