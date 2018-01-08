4be383ce113dc9aa8f3842767a8b315661dbadf4
Stuart Olivera <stuart@stuartolivera.com>
2018-01-08 01:17:05 -0500

    Notify user of incomplete application on homepage

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -20,4 +20,37 @@ class HomeControllerTest < ActionController::TestCase
     get :apilist
     assert_response :success
   end
+
+  context "while not authenticated" do
+    should "not display incomplete notice" do
+      get :index
+      assert_select ".flashes", false
+    end
+  end
+
+  context "while authenticated with a complete questionnaire" do
+    setup do
+      @questionnaire = create(:questionnaire)
+      @request.env["devise.mapping"] = Devise.mappings[:admin]
+      sign_in @questionnaire.user
+    end
+
+    should "not display incomplete notice" do
+      get :index
+      assert_select ".flashes", false
+    end
+  end
+
+  context "while authenticated with an incomplete questionnaire" do
+    setup do
+      @user = create(:user)
+      @request.env["devise.mapping"] = Devise.mappings[:admin]
+      sign_in @user
+    end
+
+    should "display incomplete notice" do
+      get :index
+      assert_select ".flashes", /You have an incomplete application/
+    end
+  end
 end
