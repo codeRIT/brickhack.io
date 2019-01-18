78ded34ef267285dbe6ae5246ce6706ebf4d963a
Stuart Olivera <stuart@stuartolivera.com>
2019-01-18 17:26:25 -0500

    Update hackathon_manager to 0.13.0 (#701)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_16_185303) do
+ActiveRecord::Schema.define(version: 2019_01_18_221503) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -250,11 +250,10 @@ ActiveRecord::Schema.define(version: 2019_01_16_185303) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.boolean "admin", default: false
-    t.boolean "admin_limited_access", default: false
     t.string "provider"
     t.string "uid"
     t.datetime "reminder_sent_at"
+    t.integer "role", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["provider"], name: "index_users_on_provider"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
