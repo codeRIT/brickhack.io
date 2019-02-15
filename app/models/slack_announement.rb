7d46b4c1aa5ffcc1c6450a305344609b63b2b6b4
dquach217 <dq6180@rit.edu>
2019-02-15 09:50:58 -0500

    grab the slack api response

---
+++ app/models/slack_announement.rb
@@ -0,0 +1,20 @@
+require 'httparty'
+SLACK_TOKEN = ENV['SLACK_TOKEN']
+CHANNEL = "CEVDW26C8".freeze
+SLACK_METHOD = "https://slack.com/api/channels.history".freeze
+COUNT = 20.freeze
+
+class Slack
+  def initialize
+    if !response || response["ok"] == false ?
+      Rails.logger.error "Error reading Slack. Response: #{sheet_data['error'].inspect}"
+      return
+    end
+    @sheet = response["messages"].map(&:text)
+  end
+
+  def response
+    response = HTTParty.get("#{SLACK_METHOD}?token=#{SLACK_TOKEN}&channel=#{CHANNEL}&count=#{COUNT}")
+    response ? response.parsed_response.to_hash : nil
+  end
+end
