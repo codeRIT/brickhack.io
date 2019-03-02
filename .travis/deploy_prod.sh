2030cb3d0d8f8c9fa3596e6b4fb25eaa6d0a012b
Stuart Olivera <stuart@stuartolivera.com>
2019-03-02 16:40:48 -0500

    Update deploy ssh key

--- .travis/deploy_prod.sh
+++ .travis/deploy_prod.sh
@@ -1,5 +1,5 @@
 eval "$(ssh-agent -s)" # start the ssh agent
-openssl aes-256-cbc -K $encrypted_60565ecba2d1_key -iv $encrypted_60565ecba2d1_iv -in .travis/marketing_rsa.enc -out .travis/marketing_rsa -d
+openssl aes-256-cbc -K $encrypted_fb724e9ad24a_key -iv $encrypted_fb724e9ad24a_iv -in .travis/marketing_rsa.enc -out .travis/marketing_rsa -d
 chmod 600 .travis/marketing_rsa # this key should have push access
 ssh-add .travis/marketing_rsa
 git remote add deploy dokku@csh-cloud.oweb.co:brickhack-prod
