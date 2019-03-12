c2ec8865c1428a067ff4125980831c3591c065ef
Stuart Olivera <stuart@stuartolivera.com>
2019-03-12 15:19:01 -0400

    Remove Sidekiq from local development (#766)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-# Ensure we don't have two containers simultaneously running "bundle install"
-until [ ! -f .bundle-install-lock ]; do
-  >&2 echo "Another container is running 'bundle install' - waiting"
-  sleep 5
-done
-
-touch .bundle-install-lock
 # Ensure all gems installed. Add binstubs to bin which has been added to PATH in Dockerfile.
 bundle check || bundle install --binstubs="$BUNDLE_BIN"
-rm -f .bundle-install-lock
 
 # Clean up a potential leftover Rails pid file
 rm -f /tmp/rails-server.pid
