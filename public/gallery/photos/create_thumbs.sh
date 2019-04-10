4cbf5d6c8d9bb7c1fd65305a8359adc1b00a8f2d
Stuart Olivera <stuart@stuartolivera.com>
2019-04-09 21:25:02 -0400

    Fix script, add documentation

--- public/gallery/photos/create_thumbs.sh
+++ public/gallery/photos/create_thumbs.sh
@@ -1,6 +1,20 @@
-#!/bin/bash
-THUMBS_FOLDER=2019/thumb/*
-for file in 2019/full/*
+#!/bin/bash -eo pipefail
+
+#
+# This script generates thumbnails for a year's gallery photos.
+# Run it like this:
+#
+#     cd /path/to/brickhack.io/public/gallery/photos/
+#     bash create_thumbs.sh
+
+# UPDATE THIS FOR THE SPECIFIC YEAR TO GENERATE
+YEAR_FOLDER=2019
+
+# Nothing below this line should need to be modified
+$FULL_FOLDER="${YEAR_FOLDER}/full"
+$THUMBS_FOLDER="${YEAR_FOLDER}/thumb"
+mkdir -p $THUMBS_FOLDER
+for file in "${FULL_FOLDER}/*"
 do
   # next line checks the mime-type of the file
   CHECKTYPE=`file --mime-type -b "$file" | awk -F'/' '{print $1}'`
@@ -10,7 +24,7 @@ do
 
     # next 'if' is true if either filesize >= 200000 bytes  OR  if image width >=201
     if [ $CHECKSIZE -ge  200000 ] || [ $CHECKWIDTH -ge 401 ]; then
-       convert -sample 400x300 "$file" "$(dirname "${THUMBS_FOLDER}")/$(basename "$file")"
+       convert -sample 400x300 "$file" "${THUMBS_FOLDER}/$(basename "$file")"
     fi
   fi
 done
