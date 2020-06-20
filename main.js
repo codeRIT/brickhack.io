24472f668f5c2359c396a9e269cce25c9b376d16
William Wu <ww2497@rit.edu>
2020-06-20 07:28:14 -0400

    Basic home layout

---
+++ main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        title: 'BRICKHACK 7',
+        description: "Brick City's Premiere Hackathon",
+        info: "Feb xx - xx | ROCHESTER INSTITUTE OF TECHNOLOGY",
+        dullblue: 'rgb(50,75,150)',
+        purple: 'rgb(125,100,200)',
+    },
+})
