9611ecd6431ba3d7c8d3780a27debbe1c0875aa2
William Wu <ww2497@rit.edu>
2020-06-20 16:45:59 -0400

    Moved the color vars to css, sry forgot to branch

--- main.js
+++ main.js
@@ -4,7 +4,5 @@ var app = new Vue({
         title: 'BRICKHACK 7',
         description: "Brick City's Premiere Hackathon",
         info: "Feb xx - xx | ROCHESTER INSTITUTE OF TECHNOLOGY",
-        dullblue: 'rgb(50,75,150)',
-        purple: 'rgb(125,100,200)',
     },
 })
