240f7a6d2b64e22eeca01eafaf50ece1bc6ee1d9
sjv5866 <sjv5866@g.rit.edu>
2020-06-27 13:50:51 -0400

    covid banner done

--- main.js
+++ main.js
@@ -4,5 +4,6 @@ var app = new Vue({
         title: 'BRICKHACK 7',
         description: "Brick City's Premiere Hackathon",
         info: "Feb xx - xx | ROCHESTER INSTITUTE OF TECHNOLOGY",
+        covid: "COVID-19 Notice: We're planning BrickHack 7 to be the best and safest environment possible. We will publish relevant information upon receiving updates from RIT and NY state.",
     },
 })
