0621d83758f57852630953408decc12e64ce1ea5
sjv01180 <samuelvelasquez@protonmail.com>
2020-06-27 16:40:41 -0400

    sticky nav attempt

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-import React from 'react';
-import { Button } from '@material-ui/core';
+//import React from 'react';
+//import { Button } from '@material-ui/core';
 
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")                                              lassList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+/*
 Vue.component('hackathon-button', {
     data: function () {
       return {
@@ -14,4 +28,4 @@ var app = new Vue({
     el: '#app',
     data: {
     },
-})
+})*/
