3868b8de45849a832d707ae52294fc31f58f3bf3
sjv01180 <samuelvelasquez@protonmail.com>
2020-07-05 15:46:10 -0400

    nav bar link sizes are relative to text length

--- src/preregister.js
+++ src/preregister.js
@@ -17,7 +17,7 @@ class PreRegister extends React.Component {
                 <div class="navigation" id="navigation">
                     <ul class="flex-container">
                         <li class="mobile-hide"><a href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf">MLH CODE OF CONDUCT</a></li>
-                        <li><a href="https://brickhack.io/gallery/">GALLERY</a></li>
+                        <a href="https://brickhack.io/gallery/"><li>GALLERY</li></a>
                     </ul>
                     <a href="https://mlh.io/seasons/na-2021/events?utm_source=na-hackathon&amp;utm_medium=TrustBadge&amp;utm_campaign=2020-season&amp;utm_content=gray">
                         <img id="mlh-badge" alt="Major League Hacking 2021 Hackathon Season" src="https://s3.amazonaws.com/logged-assets/trust-badge/2021/mlh-trust-badge-2021-black.svg"></img>
