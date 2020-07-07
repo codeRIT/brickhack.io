1ce9e7ca0e5cd51a40d9ec9949e6b1b7c572f7d1
William Wu <ww2497@rit.edu>
2020-07-07 18:02:08 -0400

    Moved files

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PreRegister from './preregister';
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+
+ReactDOM.render(
+    <PreRegister/>,
+    document.getElementById('root'),
+);
+
