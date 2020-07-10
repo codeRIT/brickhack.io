7288fa38773918ef60c7051c13d4e711958ddeb5
William Wu <ww2497@rit.edu>
2020-07-10 03:32:07 -0400

    Removed IE11 support, basic sass conversion

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PreRegister from './preregister';
 import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
+import './index.scss';
 
 ReactDOM.render(
     <PreRegister/>,
