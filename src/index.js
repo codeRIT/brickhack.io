900f162e21eb7a2ab2b208c90a6e39c41d03f2e9
sjv01180 <samuelvelasquez@protonmail.com>
2020-07-10 18:24:11 -0400

    small changes to throw away

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import PreRegister from './preregister';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
+//import './styles/style.sass'
 
 ReactDOM.render(
     <PreRegister/>,
