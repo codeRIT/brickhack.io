ea45587bd37e4935a56e7e2ed791232a4262e303
William Wu <ww2497@rit.edu>
2020-08-01 18:20:28 -0400

    Basic link to codeRIT added to the console

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import PreRegister from './preregister';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.scss';
 
+console.log("Interested in getting further involved?\n"+
+"codeRIT is looking for developers, come and join us at https://coderit.org/!");
 
 ReactDOM.render(
     <PreRegister/>,
     document.getElementById('root'),
 );
-
