d5f856d387bcc3aed6a72f1de2a4b376485c07ec
William Wu <ww2497@rit.edu>
2020-08-07 22:41:18 -0400

    Updated the console hiring message

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import PreRegister from './preregister';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.scss';
 
-console.log("Interested in getting further involved?\n"+
-"codeRIT is looking for developers, come and join us at https://coderit.org/!");
+console.log(`Hey, you.\n
+You’re finally awake.\n
+You were trying to see the code, right?\n
+Walked right into that hiring message, same as us.\n
+If you’d like to work on this website and other cool projects with hackathons, send an email over to engineering@coderit.edu!`);
 
 ReactDOM.render(
     <PreRegister/>,
