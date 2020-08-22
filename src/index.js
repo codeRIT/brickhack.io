ad712e3d6a8a7acb94b93e9750400a741b493954
ww2497 <ww2497@rit.edu>
2020-08-22 19:47:49 -0400

    Changed the console comment into an HTML comment

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import PreRegister from './preregister';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.scss';
 
-console.log(`Hey, you.\n
-You’re finally awake.\n
-You were trying to see the code, right?\n
-Walked right into that hiring message, same as us.\n
-If you’d like to work on this website and other cool projects with hackathons, send an email over to engineering@coderit.edu!`);
-
 ReactDOM.render(
     <PreRegister/>,
     document.getElementById('root'),
