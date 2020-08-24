0b7b9ba8d27b1795d765a451fb3aa3924bcde5fd
ww2497 <ww2497@rit.edu>
2020-08-24 19:48:27 -0400

    Hiring msg. is now an HTML comment & console msg

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import PreRegister from './preregister';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.scss';
 
+const hiringMessage = `Hey, you.
+You’re finally awake.
+You were trying to see the code, right?
+Walked right into that hiring message, same as us.
+If you’d like to work on this website and other cool projects with hackathons, send an email over to engineering@coderit.edu!`
+
+console.log(hiringMessage);
+
+const comment = document.createComment("\n"+hiringMessage.toString()+"\n");
+document.insertBefore(comment, document.firstChild);
+
 ReactDOM.render(
     <PreRegister/>,
     document.getElementById('root'),
