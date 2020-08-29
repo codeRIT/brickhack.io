c3523ac583205b8942ffbef48ef526b2febb2f44
William Wu <31863372+ww2497@users.noreply.github.com>
2020-08-29 18:51:00 -0400

    Update src/index.js
    Co-authored-by: Peter Kos <pkos91@icloud.com>

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const hiringMessage = `Hey, you.
 You’re finally awake.
 You were trying to see the code, right?
 Walked right into that hiring message, same as us.
-If you’d like to work on this website and other cool projects with hackathons, send an email over to engineering@coderit.edu!`
+If you’d like to work on this website and other cool projects with hackathons, send an email over to engineering@coderit.org!`
 
 console.log(hiringMessage);
 
