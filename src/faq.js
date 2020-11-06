dc73ab41492c44400bb86d5b6880c1b270b64aa1
ww2497 <ww2497@rit.edu>
2020-11-06 05:37:52 -0500

    Nav blur, shelf vertical position scaling removed, refactoring

---
+++ src/faq.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class About extends React.Component {
+    render() {
+        return(
+            <section id="about">
+            </section>
+        );
+    }
+};
+
+export default About;
