fd2162fe8720e9be0947c12101077b92fb2c28ff
ww2497 <ww2497@rit.edu>
2020-11-28 01:39:29 -0500

    Requested changes

--- src/schedule.js
+++ src/schedule.js
@@ -9,7 +9,7 @@ class Schedule extends React.Component {
 	};
 
 	time = "10-10:30am";
-	text = "Lorem ipsum dolor sit amet";
+	text = "Lorem ipsum dolor sit amet ";
 	text2 = "Bees?";
 
 	defaultColor = "#FFFFFF";
@@ -27,36 +27,46 @@ class Schedule extends React.Component {
 		var saturdayColor = this.state.selected === "SATURDAY" ? this.selectedColor : this.defaultColor;
 		var sundayColor = this.state.selected === "SUNDAY" ? this.selectedColor : this.defaultColor;
 
-		var events = this.state.selected === "SATURDAY" ? 
+		var events;
+		if (this.state.selected === "SATURDAY") {
 			// saturday events
-			<div className="events">
+			events = <div className="events">
 				<Event time={this.time} text={this.text}/>
 				<Event time={this.time} text={this.text}/>
 				<Event time={this.time} text={this.text}/>
 				<Event time={this.time} text={this.text}/>
 				<Event time={this.time} text={this.text}/> 
-			</div>
-			:
+			</div>;
+		} else {
 			// sunday events
-			<div className="events">
+			events = <div className="events">
 				<Event time={this.time} text={this.text2}/>
 				<Event time={this.time} text={this.text2}/>
 				<Event time={this.time} text={this.text2}/>
 				<Event time={this.time} text={this.text2}/>
 				<Event time={this.time} text={this.text2}/> 
 			</div>;
+		}
 
 		return(
 			<section id="schedule">
 				<div className="content">
-					<span className="title">
+					<h3 className="title">
 						Schedule
-					</span>
+					</h3>
 					<div className="schedule">
-						<div className="tape"/>
+						<div className="tape"></div>
 						<div className="dates">
-							<Day day="20" dayName="SATURDAY" color={saturdayColor} buttonClick={this.changeSelected.bind(this, "SATURDAY")}/>
-							<Day day="21" dayName="SUNDAY" color={sundayColor} buttonClick={this.changeSelected.bind(this, "SUNDAY")}/>
+							<Day day="20" 
+								dayName="SATURDAY"
+								color={saturdayColor} 
+								buttonClick={this.changeSelected.bind(this, "SATURDAY")}
+							/>
+							<Day day="21" 
+								dayName="SUNDAY" 
+								color={sundayColor} 
+								buttonClick={this.changeSelected.bind(this, "SUNDAY")}
+							/>
 						</div>
 						
 						{events}
@@ -86,12 +96,12 @@ class Event extends React.Component {
 	render() {
 		return(
 			<div className="event">
-				<div className="time">
+				<span className="time">
 					{this.props.time}
-				</div>
-				<div className="details">
+				</span>
+				<span className="details">
 					{this.props.text}
-				</div>
+				</span>
 			</div>
 		);
 	}
