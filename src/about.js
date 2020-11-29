03933a0c8a130d857b65cd33ce800600a1842f84
Shadedog838 <32343626+Shadedog838@users.noreply.github.com>
2020-11-28 22:07:05 -0500

    update style

--- src/about.js
+++ src/about.js
@@ -32,35 +32,33 @@ class About  extends React.Component {
                         </div>
                     </Slider>
                     <div class="content">
-                        <div id="header">
-                            <h2 id="title">What is a Hackathon?</h2>
-                        </div>
-                        <div id= "para">
-                            <p>A Hackathon is an invention marathon. Any student can attend! Really, any student... 
-                                and it's completely free.</p>
-                            <br></br>
-                            <p>Students work in teams and mentors are present to offer help.
-                                Sponsors attend to help, recruit, and promote their products. 
-                                Teams compete in different categories and prizes are awarded, but everyone learns and everyone wins!</p>
-                            <br></br>
-                            <p>Come spend 24 hours with us at BrickHack and dedicate time to learn, collaborate, build, and innovate.</p>
-                        </div>
+                        <h2>What is a Hackathon?</h2>
+                        <br></br>
+                        <p>A Hackathon is an invention marathon. Any student can attend! Really, any student... 
+                            and it's completely free.</p>
+                        <br></br>
+                        <p>Students work in teams and mentors are present to offer help.
+                            Sponsors attend to help, recruit, and promote their products. 
+                            Teams compete in different categories and prizes are awarded, but everyone learns and everyone wins!</p>
+                        <br></br>
+                        <p>Come spend 24 hours with us at BrickHack and dedicate time to learn, collaborate, build, and innovate.</p>
                     </div>
-                    {/* <div className="buttons">
-                        <div className= "blue_box" id ="community_box">
-                                <div className ="arrow_orange"></div>
-                                <div className ="line orange_line"></div> 
-                            <div className="text-box" id="comm_text">
-                                <p>Connect with the BrickHack community</p></div>
+                    <div class="buttons">
+                        <div class="blue_box" id="community_box">
+                                <div class ="arrow_orange"></div>
+                                <div class ="line orange_line"></div> 
+                            <div class="text-box">
+                                <p>Connect with the BrickHack community</p>
+                            </div>
                         </div>
-                        <div className ="blue_box" id ="MLH_box">
-                                <div className ="arrow_red"></div>
-                                <div className ="line red_line"></div> 
-                            <div className="text-box" id="MLH_text">
+                        <div class="blue_box" id="MLH_box">
+                                <div class="arrow_red"></div>
+                                <div class="line red_line"></div> 
+                            <div class="text-box">
                                 <p>Visit Major League Hacking (MLH)</p>
                             </div>
                         </div>
-                    </div> */}
+                    </div>
             </section>
         );
     }
