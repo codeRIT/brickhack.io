4479198475080b7b17cca299e18eb2f1865b9194
Peter Kos <pkos91@icloud.com>
2020-11-29 23:15:32 -0500

    Clip wire redo goodbye zindex you were never real
    Signed-off-by: Peter Kos <pkos91@icloud.com>

--- src/about.js
+++ src/about.js
@@ -15,63 +15,68 @@ class About  extends React.Component {
         };
         return(
             <section id="about">
-                    <div class="wire"></div>
-                    <Slider {...settings}>
-                        <div>
-                            <img class="front_clip" src={require("./bh7-assets/front_clip.svg")}/>
-                            <img class = "back_clip" src={require("./bh7-assets/back_clip.svg")}/>
-                        </div>
-                        <div>
-                            <img class="front_clip" src={require("./bh7-assets/front_clip.svg")}/>
-                            <img class="back_clip" src={require("./bh7-assets/back_clip.svg")}/>
-                        </div>
-                        <div>
-                            <img class="front_clip" src={require("./bh7-assets/front_clip.svg")}/>
-                            <img class="back_clip" src={require("./bh7-assets/back_clip.svg")}/>
-                        </div>
-                        <div>
-                            <img class="front_clip" src={require("./bh7-assets/front_clip.svg")}/>
-                            <img class = "back_clip" src={require("./bh7-assets/back_clip.svg")}/>
-                        </div>
-                        <div>
-                            <img class="front_clip" src={require("./bh7-assets/front_clip.svg")}/>
-                            <img class = "back_clip" src={require("./bh7-assets/back_clip.svg")}/>
-                        </div>
-                        <div>
-                            <img class="front_clip" src={require("./bh7-assets/front_clip.svg")}/>
-                            <img class = "back_clip" src={require("./bh7-assets/back_clip.svg")}/>
-                        </div>
-                    </Slider>
-                    <div class="content">
-                        <h2>What is a Hackathon?</h2>
-                        <br></br>
-                        <p>A Hackathon is an invention marathon. Any student can attend! Really, any student...
-                            and it's completely free.</p>
-                        <br></br>
-                        <p>Students work in teams and mentors are present to offer help.
-                            Sponsors attend to help, recruit, and promote their products.
-                            Teams compete in different categories and prizes are awarded, but everyone learns and everyone wins!</p>
-                        <br></br>
-                        <p>Come spend 24 hours with us at BrickHack and dedicate time to learn, collaborate, build, and innovate.</p>
+                <Slider {...settings}>
+                    <div class="slide">
+                        <div class="wire"></div>
+                        <div class="front_clip"></div>
+                        <div class="back_clip"></div>
                     </div>
-                    <div class="buttons-container">
-                        {/* <a href =""><div class="blue_box" id="community_box">
-                                <div class ="arrow_orange"></div>
-                                <div class ="line orange_line"></div>
-                            <div class="text-box">
-                                <p>Connect with the BrickHack community</p>
-                            </div>
-                        </div>
-                        </a> */}
-                        <a href ="https://mlh.io/" target = "_blank"><div class="blue_box" id="MLH_box">
-                                <div class="arrow_red"></div>
-                                <div class="line red_line"></div>
-                            <div class="text-box">
-                                <p>Visit Major League Hacking (MLH)</p>
-                            </div>
-                        </div>
-                        </a>
+                    <div class="slide">
+                        <div class="wire"></div>
+                        <div class="front_clip"></div>
+                        <div class="back_clip"></div>
                     </div>
+                    <div class="slide">
+                        <div class="wire"></div>
+                        <div class="front_clip"></div>
+                        <div class="back_clip"></div>
+                    </div>
+                    <div class="slide">
+                        <div class="wire"></div>
+                        <div class="front_clip"></div>
+                        <div class="back_clip"></div>
+                    </div>
+                    <div class="slide">
+                        <div class="wire"></div>
+                        <div class="front_clip"></div>
+                        <div class="back_clip"></div>
+                    </div>
+                    <div class="slide">
+                        <div class="wire"></div>
+                        <div class="front_clip"></div>
+                        <div class="back_clip"></div>
+                    </div>
+                </Slider>
+                <div class="content">
+                    <h2>What is a Hackathon?</h2>
+                    <br></br>
+                    <p>A Hackathon is an invention marathon. Any student can attend! Really, any student...
+                        and it's completely free.</p>
+                    <br></br>
+                    <p>Students work in teams and mentors are present to offer help.
+                        Sponsors attend to help, recruit, and promote their products.
+                        Teams compete in different categories and prizes are awarded, but everyone learns and everyone wins!</p>
+                    <br></br>
+                    <p>Come spend 24 hours with us at BrickHack and dedicate time to learn, collaborate, build, and innovate.</p>
+                </div>
+                <div class="buttons-container">
+                    {/* <a href =""><div class="blue_box" id="community_box">
+                            <div class ="arrow_orange"></div>
+                            <div class ="line orange_line"></div>
+                        <div class="text-box">
+                            <p>Connect with the BrickHack community</p>
+                        </div>
+                    </div>
+                    </a> */}
+                    <a href ="https://mlh.io/" target = "_blank"><div class="blue_box" id="MLH_box">
+                            <div class="arrow_red"></div>
+                            <div class="line red_line"></div>
+                        <div class="text-box">
+                            <p>Visit Major League Hacking (MLH)</p>
+                        </div>
+                    </div>
+                    </a>
+                </div>
             </section>
         );
     }
