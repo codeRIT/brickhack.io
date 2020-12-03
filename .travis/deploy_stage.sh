324677f9027366f011b269671cb8c819b37eb4cd
Peter Kos <pkos91@icloud.com>
2020-12-03 01:57:41 -0500

    Remove .buildpacks + leftover travis files
    Signed-off-by: Peter Kos <pkos91@icloud.com>

--- .travis/deploy_stage.sh
+++
@@ -1,7 +0,0 @@
-eval "$(ssh-agent -s)" # start the ssh agent
-openssl aes-256-cbc -K $encrypted_fb724e9ad24a_key -iv $encrypted_fb724e9ad24a_iv -in .travis/marketing_rsa.enc -out .travis/marketing_rsa -d
-chmod 600 .travis/marketing_rsa # this key should have push access
-ssh-add .travis/marketing_rsa
-git remote add deploy dokku@csh-cloud.oweb.co:brickhack-stage
-git fetch --unshallow
-git push --force deploy develop:master
