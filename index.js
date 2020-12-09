04004441177e6932fa654219bd6a0c62fc65e736
Peter Kos <pkos91@icloud.com>
2020-12-09 03:48:36 -0500

    Start of slider
    - Add jQuery
    - Add slick-slider
    
    Signed-off-by: Peter Kos <pkos91@icloud.com>

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 
 
-import "./sass/main.scss"
+import './sass/main.scss'
 import '@fortawesome/fontawesome-free/css/all.css'
+
+
+// slick-carousel configuration
+
+import $ from 'jquery'
+import 'slick-carousel'
+
+$(document).ready(function() {
+    $('.carousel').slick({
+        infinite: true,
+        speed: 500,
+        swipeToSlide: true,
+        variableWidth: true,
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        autoplay: true,
+        autoplaySpeed: 500
+    });
+});
