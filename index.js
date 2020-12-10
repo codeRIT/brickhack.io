126c7e68803a4c85cfdaa02ae8766d3196f8fa01
cxs7700 <cxs7700@g.rit.edu>
2020-12-10 04:15:23 -0500

    added js for accordion

--- index.js
+++ index.js
@@ -22,3 +22,18 @@ $(document).ready(function() {
         speed: 1000,
     });
 });
+
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
