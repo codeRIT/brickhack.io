6dde94bbd068981ddd61ae5f0e7f83a36ed90b52
cxs7700 <cxs7700@g.rit.edu>
2020-12-10 05:03:23 -0500

    finishing touches on icons

--- index.js
+++ index.js
@@ -27,13 +27,15 @@ let acc = document.getElementsByClassName("accordion");
 let i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    let panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+            this.getElementsByTagName("i")[0].style.display = "inline-block";
+        } else {
+            panel.style.display = "block";
+            this.getElementsByTagName("i")[0].style.display = "none";
+        }
+    });
 }
