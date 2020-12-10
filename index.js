d9d2e7697010dc350e810d04bde47830cc831fbf
cxs7700 <cxs7700@g.rit.edu>
2020-12-10 15:25:59 -0500

    removed noopener

--- index.js
+++ index.js
@@ -24,9 +24,7 @@ $(document).ready(function() {
 });
 
 let acc = document.getElementsByClassName("accordion");
-let i;
-
-for (i = 0; i < acc.length; i++) {
+for (let i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
         this.classList.toggle("active");
         let panel = this.nextElementSibling;
