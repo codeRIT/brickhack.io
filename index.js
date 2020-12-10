7b8ae0225f640bf3ce7bf1efab43b3c18d63db8f
Peter Kos <pkos91@icloud.com>
2020-12-10 00:24:10 -0500

    Photo carousel for general hackathon section (#986)
    * Start of slider
    
    - Add jQuery
    - Add slick-slider
    
    Signed-off-by: Peter Kos <pkos91@icloud.com>
    
    * Lots of slider progress
    
    Signed-off-by: Peter Kos <pkos91@icloud.com>
    
    * Progress on clip tweaks; will do more tomorrow
    
    Signed-off-by: Peter Kos <pkos91@icloud.com>
    
    * Tweaks
    
    Signed-off-by: Peter Kos <pkos91@icloud.com>
    
    * Tweak z-index to be simpler (hopefully), dummy img
    
    Signed-off-by: Peter Kos <pkos91@icloud.com>
    
    * Clip size tweaks
    
    Signed-off-by: Peter Kos <pkos91@icloud.com>
    
    * Added in photos!
    
    Signed-off-by: Peter Kos <pkos91@icloud.com>

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 
 
-import "./sass/main.scss"
+import './sass/main.scss'
 import '@fortawesome/fontawesome-free/css/all.css'
+
+
+// slick-carousel configuration
+
+import $ from 'jquery'
+import 'slick-carousel'
+
+$(document).ready(function() {
+    $('.carousel').slick({
+        infinite: true,
+        swipeToSlide: true,
+        variableWidth: true,
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        cssEase: 'ease-in-out',
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 1000,
+    });
+});
