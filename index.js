2e1b37131c86dba0c213c85c1ac6bc72b1e9a5bf
Colby Gallup <colbygallup@gmail.com>
2020-12-16 16:21:43 -0600

    Schedule is now pulled from HackathonManager

--- index.js
+++ index.js
@@ -64,6 +64,53 @@ for (let i = 0; i < card.length; i++) {
     });
 }
 
+// Dynamic schedule code (sample API data from test_data.json)
+function convertDate(date) {
+    let output = '';
+
+    // hour
+    if (date.getUTCHours() > 12) {
+        output = String(date.getUTCHours() - 12);
+    } else {
+        output = String(date.getUTCHours());
+    }
+
+    // minute
+    if (date.getMinutes() !== 0) {
+        output += ':' + String(date.getUTCMinutes()).padStart(2, '0');
+    }
+
+    // AM/PM
+    if (date.getUTCHours() >= 12) {
+        output += 'pm';
+    } else {
+        output += 'am';
+    }
+
+    return output;
+}
+
+const events = [{"title":"Opening Ceremony","description":"","location":"Discord + Zoom","start":"1613815200","finish":"1613817000"},{"title":"Lunch (on your own!)","description":"","location":"Discord + Zoom","start":"1613822400","finish":"1613822400"},{"title":"Mystery Workshop","description":"","location":"Discord + Zoom","start":"1613829600","finish":"1613919600"},{"title":"Mystery Event","description":"","location":"Discord + Zoom","start":"1613840400","finish":"1613844000"},{"title":"Devpost submission","description":"","location":"Discord + Zoom","start":"1613901600","finish":"1613901600"},{"title":"Mystery Workshop 2","description":"","location":"Discord + Zoom","start":"1613912400","finish":"1613916000"},{"title":"Coding stops / Judging begins","description":"","location":"Discord + Zoom","start":"1613910600","finish":"1613916000"},{"title":"Closing Ceremony","description":"","location":"Discord + Zoom","start":"1613916000","finish":"1613923200"}];
+events.forEach(event => {
+    let startDate = new Date(Number(event.start) * 1000);  // convert timestamp -> milliseconds -> Date object
+    let finishDate = new Date(Number(event.finish) * 1000);
+    
+    let dateString = convertDate(startDate);
+    if (startDate.getTime() !== finishDate.getTime()) {  // instantaneous events don't need the second part
+        let finishString = convertDate(finishDate);
+        if (dateString.slice(-2) === finishString.slice(-2)) {  // hide "am/pm" of first time if both are identical
+            dateString = dateString.slice(0, -2);
+        }
+        dateString += "-" + convertDate(finishDate);
+    }
+
+    let eventContainer = $('.day-first-events');
+    if (startDate.getDate() === 21) {
+        eventContainer = $('.day-second-events');
+    }
+    eventContainer.append(`<div class="event"><p class="time">${dateString}</p><p class="title">${event.title}</p></div>`);
+});
+
 // Schedule toggle code
 $('.day-second-events').hide();
 $('.day-first').click(function() {
