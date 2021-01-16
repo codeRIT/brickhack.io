4677352b067bdf7270e4e6fffe5f060f2c9fd670
sjmiller7 <sj.miller.tn@gmail.com>
2021-01-16 00:21:23 -0500

    making the modal open/close

--- gallery.js
+++ gallery.js
@@ -1,2 +1,24 @@
 import './sass/gallery.scss'
 import '@fortawesome/fontawesome-free/css/all.css'
+
+import $ from 'jquery'
+
+//Opening modal
+$(document).on('click', function(event) {
+    if ($(event.target).attr('class') == 'image') {
+        $('#modal').show();
+        $('#modal-background').show();
+    }
+});
+
+//Closing modal with x
+$('#close-modal').on('click', function() {
+    $('#modal').hide();
+    $('#modal-background').hide();
+});
+
+//Closing modal with background
+$('#modal-background').on('click', function() {
+    $('#modal').hide();
+    $('#modal-background').hide();
+});
