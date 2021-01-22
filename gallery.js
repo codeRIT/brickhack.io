f09bd8b91d97d6ffd0098de7ac918a64dcb787c7
sjmiller7 <56453920+sjmiller7@users.noreply.github.com>
2021-01-21 23:32:20 -0500

    Gallery Image Popup (#1107)
    * adding html/css for modal
    
    * making the modal open/close
    
    * fixing stuff
    
    * scss fixes
    
    * fixes
    
    * fixing the modal
    
    * fixes

--- gallery.js
+++ gallery.js
@@ -3,18 +3,41 @@ import '@fortawesome/fontawesome-free/css/all.css'
 
 import $ from 'jquery'
 
+// Opening modal
+$(document).on('click', function(event) {
+    if ($(event.target).attr('class') == 'image') {
+        $('#modal').show();
+        var top = 'calc(5% + ' + (window.scrollY) + 'px)';
+        $('#modal').css('top', top);
+        $('#modal-background').show();
+    }
+});
+
+// Closing modal with x
+$('#close-modal').on('click', function() {
+    $('#modal').hide();
+    $('#modal-background').hide();
+});
+
+// Closing modal with background
+$('#modal-background').on('click', function() {
+    $('#modal').hide();
+    $('#modal-background').hide();
+});
+
 // Navbar functionality
 $(document).on('click', '#toggler', function() {
     if ($('.navlinks').hasClass('show-menu')) {
         $('.navlinks').removeClass('show-menu');
         $('#toggler').removeClass('fa-times');
         $('#toggler').addClass('fa-bars');
-    } else {;
+    } else {
         $('.navlinks').addClass('show-menu');
         $('#toggler').removeClass('fa-bars');
         $('#toggler').addClass('fa-times');
     }
 });
+
 // Closing the navbar when a navigation link is clicked
 $(window).on('click', function(event) {
     if ($(event.target).hasClass('navlink')) {
