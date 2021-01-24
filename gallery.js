4b7e01e118db3e55f225c8795e9cf0a5e84aac6d
sjmiller7 <sj.miller.tn@gmail.com>
2021-01-24 01:48:16 -0500

    comment fix

--- gallery.js
+++ gallery.js
@@ -41,6 +41,7 @@ function viewAlbum(albumName) {
         var photos = data.Contents.map(function(photo) {
             var photoKey = photo.Key;
             var photoUrl = bucketUrl + encodeURIComponent(photoKey);
+            //Seeing if there is actually an image (amazon adds a blank to count the number of images at the beginning)
             if (photo.Size == 0) {
                 return;
             }
