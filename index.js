32d03b355d22a100768257846e207e5e364e6938
Peter Kos <pkos91@icloud.com>
2021-07-02 20:35:07 -0400

    Release BH8 Preregistration Site (#1205)
    * Fix scaling and responsiveness, Safari bug (#1196)
    
    * Setup structure for post event
    
    * Start of designs
    
    * Gif added
    
    * Fix the thing
    
    * Sizing
    
    * last things
    
    * Fix scaling issues on desktop and safari
    
    * Fix a lot of responsive
    
    * BH8 windows87 prereg style website
    
    * Rename old site event.html -> bh7.html
    
    * Rename css correctly
    
    * Remove commented out styling
    
    * Style fixes and alt tags; fix gap issue on safari
    
    * Move to Netlify for staging+PRs! Remove staging GHA script + peek.yml (#1202)
    
    * Remove staging GHA script + peek.yml
    
    Using Netlify for staging PRs
    
    * Empty
    
    * Only run build on prod push, not on develop
    
    * Empty
    
    * Convert SCSS->CSS vars for mobile styling
    
    * Many responsive tweaks
    
    Notable is the height constraint added tot he media query, and width->max-width to allow for better mobile resizing
    
    * Tweak window again
    
    Normally i'd use a % but window glitches when too small and imo cutoff is better
    
    * PR feedback
    
    * Comments and alt tags
    
    * gifs are local fiels now instead of hotlinked
    
    * Mobile fixes and scaling
    
    * Fix social card and remove unused font
    
    * Update sass/index.scss
    
    Co-authored-by: Claire Gallup <colbygallup@gmail.com>
    
    * Update sass/index.scss
    
    Co-authored-by: Claire Gallup <colbygallup@gmail.com>
    
    * Update sass/index.scss
    
    Co-authored-by: Claire Gallup <colbygallup@gmail.com>
    
    * Update sass/index.scss
    
    Co-authored-by: Claire Gallup <colbygallup@gmail.com>
    
    * Update sass/index.scss
    
    Co-authored-by: Claire Gallup <colbygallup@gmail.com>
    
    * Fix sponsor email
    
    * BH8 Preregistration: Window easter egg (#1203)
    
    * Moving window!
    
    * Update browserlist cache
    
    * Fix footer dispapearing on mobile
    
    * Fix unused code move to todo
    
    * newline EOF
    
    * Update index.js
    
    Co-authored-by: Claire Gallup <colbygallup@gmail.com>
    
    * Update index.js
    
    Co-authored-by: Claire Gallup <colbygallup@gmail.com>
    
    * De semicolonification, fix out of range bug
    
    Co-authored-by: Claire Gallup <colbygallup@gmail.com>
    
    * Add Mailchimp submit form (#1204)
    
    * Altered Mailchimp code
    
    * bh rit -> BrickHack Interest audience
    
    Co-authored-by: Claire Gallup <colbygallup@gmail.com>
    Co-authored-by: Chris Baudouin, Jr <cjb5326@rit.edu>

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-import './sass/event.scss'
-import '@fortawesome/fontawesome-free/css/all.css'
+import './sass/index.scss'
+
+import { AniX } from 'anix'
+
+const $ = require('jquery')
+window.$ = window.jQuery = $
+require("jquery-ui-bundle")
 
 // Hiring message
 const hiringMessage = `Hey, you.
@@ -8,9 +13,90 @@ You were trying to see the code, right?
 Walked right into that hiring message, same as us.
 If you’d like to work on this website and other cool projects with hackathons, send an email over to engineering@coderit.org!`
 
-console.log(hiringMessage);
+console.log(hiringMessage)
 
 // Comment generated via js instead of directly in HTML so the hiring message text is only in one place
-const comment = document.createComment("\n"+hiringMessage.toString()+"\n");
-document.insertBefore(comment, document.firstChild);
+const comment = document.createComment("\n"+hiringMessage.toString()+"\n")
+document.insertBefore(comment, document.firstChild)
 
+// Secret messages
+const revertDuration = 300
+var messageIndex = 0
+const messages = [
+    "Like, really, nothing to see here.",
+    "Hey. Stop looking.",
+    "Hey. Stop looking.",
+    "...",
+    "I'm warning you...",
+    "There's really nothing.",
+    "There's really nothing.",
+    "There's really nothing.",
+    "...",
+    "Wow, you just don't want to believe me.",
+    "What have I ever done to you?!",
+    "...",
+    "Oh, right...",
+    "...the bricks...",
+    "...",
+    "Yeah, that was... my bad.",
+    "Life's hard as a rockstar, you know?",
+    "I'm really a brickstar, actually.",
+    "... leader of ThunderClan? I don't get the reference.",
+    "Have you heard my new song?",
+    "It goes like,",
+    "I'm bringin' Ricky Back! (yeah)",
+    "(barrrw) Them other boys don't know--",
+    "I'M BRINGIN' RICKY BACK (yeah)",
+    "It's a bop, if I do say so myself.",
+    "Oh, by the way Dave,",
+    "We're contacting you about your car's extended warrany...",
+    "Haha, just kidding.",
+    "I have some brick business to get back to.",
+    "You know how it is.",
+    "I do have a tip for you though.",
+    "When you sign up for BrickHack...",
+    "...make sure to ask if you need your own bricks.",
+    "Heh. Gets them every year.",
+    "Well, see ya!",
+    "(the wall is quiet.)",
+    "(it fills you with determination.)",
+    ""
+]
+
+$(document).ready(function() {
+    $(".window").draggable({
+        cursor: "grab",
+        revert: true,
+        revertDuration: revertDuration,
+        handle: ".title-bar",
+        // Adjust scale factor of page to match mouse dragging position
+        drag: function(event, ui) {
+            ui.position.top = Math.round(ui.position.top / 1.5)
+            ui.position.left = Math.round(ui.position.left / 1.5)
+        }
+    })
+})
+
+$(".window-control").click(function() {
+    $(".window").css({"visibility":"hidden"})
+})
+
+$(".title-bar").mouseup(function() {
+    // Don't show the message before the window returns to its original position
+    setTimeout(function() {
+        const offset = $(".window").position()
+        updateSecretMessage(offset)
+    }, revertDuration)
+})
+
+function updateSecretMessage(offset) {
+    // TODO: Make sure the window was dragged enough to see the message
+    // so the user has less chance of missing it before we refresh.
+
+    if (messageIndex >= messages.length) {
+        return
+    }
+
+    $("#secret-message").text(messages[messageIndex])
+    messageIndex++
+}
