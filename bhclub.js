70a2bb274c2a1ce76eecc8adf56e32a877bd7c3e
SJ Miller <sj.miller.tn@gmail.com>
2021-07-27 22:27:01 -0400

    about section + cards

--- bhclub.js
+++ bhclub.js
@@ -32,3 +32,27 @@ $(document).on('click', 'main', function() {
     $('#toggle').addClass('fa-bars');
     $('main').removeClass('main-pointer');
 });
+
+// FAQ Cards hide/show
+let card = document.getElementsByClassName("card");
+for (let i = 0; i < card.length; i++) {
+    let accordion = card[i].getElementsByClassName("card-header")[0];
+    // Click should only work on accordion-header of each card
+    accordion.addEventListener("click", function() {
+
+        card[i].classList.toggle("card-open");
+
+        let panel = card[i].getElementsByClassName("card-body")[0];
+        let fa = this.getElementsByTagName("i")[0];
+
+        // Toggle panel and plus/minus on click of header
+        if ($(card[i]).hasClass("card-open")) {
+            $(panel).slideDown(200);
+        } else {
+            $(panel).slideUp(200);
+        }
+
+        $(fa).toggleClass("fa-plus");
+        $(fa).toggleClass("fa-minus");
+    });
+}
