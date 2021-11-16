1566c9628efbf4d28bf4ae8f4986b18ad0de6fb6
SJ Miller <sj.miller.tn@gmail.com>
2021-11-16 03:21:54 -0500

    design fixes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const hiringMessage = `Hey, you.
 You’re finally awake.
 You were trying to see the code, right?
 Walked right into that hiring message, same as us.
-If you’d like to work on this website and other cool projects with hackathons, send an email over to engineering@coderit.org!`
+If you’d like to work on hackathon-related projects, check out https://brickhack.io/club!`
 
 console.log(hiringMessage);
 
