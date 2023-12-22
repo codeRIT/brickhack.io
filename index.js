bf0628ffb1b46b356b9bb9a74f72589371c5140f
Peter Kos <pkos91@icloud.com>
2023-12-22 02:30:36 -0500

    Spruce up the landing page more

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import "./sass/index.scss"
 import "@fortawesome/fontawesome-free/css/all.css"
-import $ from "jquery"
 
 // Hiring message
 const hiringMessage = `Hey, you.
@@ -14,35 +13,3 @@ console.log(hiringMessage);
 // Comment generated via js instead of directly in HTML so the hiring message text is only in one place
 const comment = document.createComment("\n" + hiringMessage + "\n");
 document.insertBefore(comment, document.firstChild);
-
-
-// Navbar functionality
-$(document).on("click", "#toggle", function() {
-    if ($("nav").hasClass("show-nav")) {
-        $("nav").removeClass("show-nav");
-        $("#toggle").removeClass("fa-times");
-        $("#toggle").addClass("fa-bars");
-        $(".mobile-grayout").removeClass("show-gray");
-    } else {
-        $("nav").addClass("show-nav");
-        $("#toggle").removeClass("fa-bars");
-        $("#toggle").addClass("fa-times");
-        $(".mobile-grayout").addClass("show-gray");
-    }
-});
-
-// Closing the navbar when a navigation link is clicked
-$(document).on("click", ".link", function() {
-    $("nav").removeClass("show-nav");
-    $("#toggle").removeClass("fa-times");
-    $("#toggle").addClass("fa-bars");
-    $(".mobile-grayout").removeClass("show-gray");
-});
-
-// Closing the navbar when outside of the nav is clicked
-$(document).on("click", ".mobile-grayout", function() {
-    $("nav").removeClass("show-nav");
-    $("#toggle").removeClass("fa-times");
-    $("#toggle").addClass("fa-bars");
-    $(".mobile-grayout").removeClass("show-gray");
-});
